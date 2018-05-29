b10c902ae810fb1688494eae10068c5ee952ac3e
Patrick R <1810057@bart.ca>
2018-05-28 20:45:07 -0400

    Image

--- JeuEchec/JeuEchec/JeuEchec/main.cpp
+++ JeuEchec/JeuEchec/JeuEchec/main.cpp
@@ -2,6 +2,80 @@
 #include <SDL.h>
 using namespace std;
 
+
+
+//Starts up SDL and creates window
+bool init();
+//Loads media
+bool loadMedia();
+//Frees media and shuts down SDL
+void close();
+
+int SCREEN_WIDTH = 550;
+int SCREEN_HEIGHT = 550;
+
+//The window we'll be rendering to
+SDL_Window* gWindow = NULL;
+//The surface contained by the window
+SDL_Surface* gScreenSurface = NULL;
+//The image we will load and show on the screen
+SDL_Surface* gBoard = NULL;
+
+bool init()
+{
+	//Initialization flag
+	bool success = true;
+	//Initialize SDL
+	if (SDL_Init(SDL_INIT_VIDEO) < 0)
+	{
+		printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+		success = false;
+	}
+	else
+	{
+		//Create window
+		gWindow = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+		if (gWindow == NULL)
+		{
+			printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+			success = false;
+		}
+		else
+		{
+			//Get window surface
+			gScreenSurface = SDL_GetWindowSurface(gWindow);
+		}
+	}
+	return success;
+}
+
+bool loadMedia()
+{
+	//Loading success flag
+	bool success = true;
+	//Load splash image
+	gBoard = SDL_LoadBMP("Images/ChestBoard.bmp");
+
+	if (gBoard == NULL)
+	{
+		printf("Unable to load image %s! SDL Error: %s\n", "Images/ChestBoard.bmp", SDL_GetError());
+		success = false;
+	}
+	return success;
+}
+
+void close()
+{
+	//Deallocate surface
+	SDL_FreeSurface(gBoard);
+	gBoard = NULL;
+	//Destroy window
+	SDL_DestroyWindow(gWindow);
+	gWindow = NULL;
+	//Quit SDL subsystems
+	SDL_Quit();
+}
+
 int main(int argc, char * argv[])
 {
 	if (SDL_Init(SDL_INIT_EVERYTHING) < 0)
@@ -12,6 +86,33 @@ int main(int argc, char * argv[])
 	{
 		cout << "SDL initialization succeeded!";
 	}
+
+
+	//Start up SDL and create window
+	if (!init())
+	{
+		printf("Failed to initialize!\n");
+	}
+	else
+	{
+		//Load media
+		if (!loadMedia())
+		{
+			printf("Failed to load media!\n");
+		}
+		else
+		{
+			//Apply the image
+			SDL_BlitSurface(gBoard, NULL, gScreenSurface, NULL);
+			//Update the surface
+			SDL_UpdateWindowSurface(gWindow);
+			//Wait two seconds
+			SDL_Delay(2000);
+		}
+	}
+	//Free resources and close SDL
+	close();
+
 
 	cin.get();
 	return 0;
